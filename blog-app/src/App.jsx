a3a386ff9d0dbd2b53a7cdbd9f0627c09f2b7482
Bishal Bhatta <bishalbhatta1028@gamil.com>
2024-05-17 16:27:49 +0545

    initilize blog appi

---
+++ blog-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="text-red-400 bg-red">hello world</div>
+    </>
+  )
+
+}
+
+export default App
