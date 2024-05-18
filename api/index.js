117632071692fd35014af8ddd0f227b2ad96f8f3
Bishal Bhatta <bishalbhatta1028@gamil.com>
2024-05-18 22:30:30 +0545

    update the backend

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import mongoose from "mongoose";
+mongoose
+  .connect("mongodb://127.0.0.1:27017/blog")
+  .then(() => console.log("Connected!"));
+
 const app = express();
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
