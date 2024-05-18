a5a39a741348f7feef51a8a172c206536a71b9d0
Bishal Bhatta <bishalbhatta1028@gamil.com>
2024-05-18 15:26:59 +0545

    routes configure

--- blog-app/src/App.jsx
+++ blog-app/src/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Projects from './pages/Projects'
 
 function App() {
   return (
     <>
-      <div className="text-red-400 bg-red">hello world</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/signUp" element={<SignIn />} />
+          <Route path="/signIn" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 
